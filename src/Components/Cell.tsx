f12c85d46ac7352e6321c013526784f2955f69c8
Shamyralieva Zeinep <zeinepshamyralieva01@gmail.com>
2024-09-23 02:37:42 +0600

    Created a Cell component.

---
+++ src/Components/Cell.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { CellProps } from '../types';
+
+const Cell: React.FC<CellProps> = ({ cell, index, onClick }) => {
+    return (
+        <div
+            className={`cell ${cell.clicked ? 'clicked' : ''}`}
+            onClick={() => onClick(index)}
+        >
+            {cell.clicked && cell.hasItem ? '💎' : ''}
+        </div>
+    );
+};
+
+export default Cell;
